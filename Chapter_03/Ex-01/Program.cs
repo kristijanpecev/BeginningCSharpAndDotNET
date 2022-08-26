c1051c37413de6d01eb39e675adfb5537274df5d
kristijanpecev <kpecev77@gmail.com>
2022-08-26 19:32:29 +0200

    Modified

---
+++ Chapter_03/Ex-01/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int myInteger;
+string myString;
+myInteger = 17;
+myString = "\"myInteger\"  is";
+Console.WriteLine($"{myString} {myInteger}");
+Console.ReadKey();
+
